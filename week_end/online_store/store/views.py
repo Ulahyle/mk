0228575b4aaae66a997c0041826326a8a0e722b8
ula <ulahyle@gmail.com>
2025-07-09 14:16:14 +0330

    setup viewswets for store app

--- week_end/online_store/store/views.py
+++ week_end/online_store/store/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from store.models import Product, Customer
+from store.serializers import ProductSerializer, CustomerSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer

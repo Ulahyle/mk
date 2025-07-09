9abbb15e8cb0f9fd83f39053706c464dcb6be573
ula <ulahyle@gmail.com>
2025-07-09 14:07:20 +0330

    setup serializer for store app

---
+++ week_end/online_store/store/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from store.models import Product, Customer
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
